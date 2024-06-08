a28cd52d6c5df7a091aaf3a4da14e64c9805fc33
Croos3r <dorian.moy@epitech.eu>
2024-06-08 20:42:09 +0200

    style: fixed small code style issues thanks to biome and added script to run it easily in package.json
    Took 16 minutes

--- zessentials-client/nuxt.config.ts
+++ zessentials-client/nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
-  shadcn: {
-    prefix: '',
-    componentDir: './components/ui'
-  }
-})
+	devtools: { enabled: true },
+	modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+	shadcn: {
+		prefix: "",
+		componentDir: "./components/ui",
+	},
+});
